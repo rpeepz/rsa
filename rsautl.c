9f8607605118aa94ea43ea4d170a9ab0b4d2ee81
rpeepz <r.papagna.42@gmail.com>
2019-12-05 18:17:49 -0800

    all functionality for project

--- rsautl.c
+++ rsautl.c
@@ -1,68 +1,106 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rsautl.h                                           :+:      :+:    :+:   */
+/*   rsautl.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpapagna <rpapagna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 13:21:29 by rpapagna          #+#    #+#             */
-/*   Updated: 2019/11/17 21:03:49 by rpapagna         ###   ########.fr       */
+/*   Updated: 2019/12/05 18:17:26 by rpapagna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rsa.h"
 
-void			rsa_decrypt(t_rsa_out rsa, t_rsa gg, char *buf, int len)
+void			print_row_hex(int bytes, int len, __uint8_t *msg, int fd)
 {
-	__uint64_t	decrypt[len];
-	int			i;
+	char	*hex;
+	int		i;
 
-	for (i = 0; i < len; i++) { 
-		ft_printf("%c", buf[i]);
-	}
-	ft_printf("\n");
+	hex = "0123456789abcdef";
 	i = 0;
-	while (i < len)
+	if (bytes < 16)
+		ft_putstr_fd("0000 - ", fd);
+	while (i < 16)
 	{
-		decrypt[i] = powmod(buf[i], gg.d, gg.n);
+		if (i < len)
+		{
+			ft_putchar_fd(hex[(msg[i] / 16)], fd);
+			ft_putchar_fd(hex[(msg[i] % 16)], fd);
+		}
+		else
+			write(1, "  ", 2);
 		++i;
-	}
-	for (i = 0; i < len; i++) { 
-		ft_printf("%c", decrypt[i]);
+		if (i == 8)
+			ft_putchar_fd('-', fd);
+		ft_putchar_fd(' ', fd);
 	}
 }
 
-void			rsa_encrypt(t_rsa_out rsa, t_rsa gg, char *buf, int len)
+static void		hex_dump(int fd, int size, int len, __uint64_t msg)
 {
-	__uint64_t	encrypt;
+	__uint8_t	s[len];
 	int			i;
+	int			j;
 
-	for (i = 0; i < len; i++) { 
-		ft_printf("%c", buf[i]);
-	}
-	ft_printf("\n");
 	i = 0;
-	encrypt = 0;
+	s[len] = 0;
 	while (i < len)
 	{
-		encrypt <<= 8;
-		encrypt += buf[i];
+		s[i] = (char)(msg >> (size - (i * 8)));
 		++i;
 	}
-	encrypt = powmod(encrypt, gg.e, gg.n);
-	for (i = 0; i < len; i++) {
-		char cha = encrypt >> (56 - (i * 8));
-		ft_printf("%c", cha);
+	i = 0;
+	while (i < len)
+	{
+		print_row_hex(i, len - i, s + i, fd);
+		j = 0;
+		while (j < 16 && j < len - i)
+		{
+			ft_putchar_fd(ft_isprint(s[i + j]) ? s[i + j] : '.', fd);
+			j++;
+		}
+		ft_putchar_fd('\n', fd);
+		i += 16;
 	}
-	ft_printf("\n");
+}
+
+static void		rsa_crypt(t_rsa_out rsa, t_rsa gg, __uint8_t *buf, int len)
+{
+	__uint64_t	msg;
+	int			size;
+	int			i;
+
+	i = 0;
+	msg = 0;
+	while (i < len)
+	{
+		msg <<= 8;
+		msg += buf[i++];
+	}
+	msg = powmod(msg, rsa.flag & R_DECRYPT ? gg.d : gg.e, gg.n);
+	i = 0;
+	size = (len * 8) - 8;
+	if (rsa.flag & R_HEXDUMP)
+		hex_dump(rsa.fd_out, size, len, msg);
+	else
+	{
+		while (i < len)
+		{
+			ft_putchar_fd((char)(msg >> (size - (i * 8))), rsa.fd_out);
+			++i;
+		}
+	}
 }
 
 void			rsautl(t_rsa_out rsa, t_rsa gg)
 {
 	__uint64_t		tmp;
 	int				len;
-	char			buf[16];
+	__uint8_t		buf[16];
 
+	if (!gg.n)
+		return ;
 	rsa.bits = 0;
 	ft_bzero(buf, 16);
 	tmp = gg.n;
@@ -72,11 +110,5 @@ void			rsautl(t_rsa_out rsa, t_rsa gg)
 	if (rsa.bits >> 3 != len)
 		ft_error(len < rsa.bits >> 3 ? 21 : 22, NULL, NULL);
 	else
-	{
-		if (rsa.flag & R_DECRYPT)
-			rsa_decrypt(rsa, gg, buf, len);
-		else if (rsa.flag & R_ENCRYPT)
-			rsa_encrypt(rsa, gg, buf, len);
-	}
-
+		rsa_crypt(rsa, gg, buf, len);
 }
